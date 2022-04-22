e7f9fdf283dddc0369bdcb57163207bbf0b535b4
cherkiral <kirill.tcherniak@ya.ru>
2022-04-22 19:59:23 +0300

    1

--- final.py
+++ final.py
@@ -12,29 +12,31 @@ with open('auth_data_temp.txt') as f:
     data = json.load(f)
 with open('users_tag.txt') as f:
     users = f.read().split()
-users_list = ['@' + user for user in users]
 
-for i in range(5):
-    users_list_temp = users_list
-    rand_user = users_list[random.randint(0, len(users_list) - 1)]
-    print(rand_user)
-    users_list.pop(users_list.index(rand_user))
 
-# for key in range(1, 200):
-#     try:
-#         print(key)
-#         follow_user(data[f'{key}'], '1485307892672700421')
-#         time.sleep(random.uniform(0, 3))
-#         follow_user(data[f'{key}'], '4017596542')
-#         time.sleep(random.uniform(0, 3))
-#         retweet_post(data[f'{key}'], '1517443100628635648')
-#         time.sleep(random.uniform(0, 3))
-#         like_post(data[f'{key}'], '1517443100628635648')
-#         time.sleep(random.uniform(0, 3))
-#         comment_tweet(data[f'{key}'], '', '1517443100628635648')
-#
-#
-#     except Exception as err:
-#         print(f"ОШИБКА В ЦИКЛЕ: {err}")
-#
-# print("--- %s seconds ---" % (time.time() - start_time))
+for key in range(1, 200):
+    users_list = ['@' + user for user in users]
+    rand_user_str = ''
+    try:
+        print(key)
+        follow_user(data[f'{key}'], '1500259433674416130')
+
+        follow_user(data[f'{key}'], '1507713106469961734')
+
+        retweet_post(data[f'{key}'], '1517387092711596034')
+
+        like_post(data[f'{key}'], '1517387092711596034')
+
+        for i in range(3):
+            rand_user = users_list[random.randint(0, len(users_list) - 1)]
+            rand_user_str += rand_user + ' '
+            users_list.pop(users_list.index(rand_user))
+        comment_tweet(data[f'{key}'], rand_user_str, '1517387092711596034')
+
+
+    except Exception as err:
+        print(f"ОШИБКА В ЦИКЛЕ: {err}")
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#time.sleep(random.uniform(0, 1))
--- test.py
+++ test.py
@@ -3,5 +3,3 @@ from pprint import pprint
 import json
 from auth_grabber import *
 
-a = 1
-raise ValueError('Could not authenticate you.')
