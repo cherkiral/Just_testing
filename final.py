322054ce0fcc2354ad4a4fa0ef3d145be3ce2e9e
cherkiral <kirill.tcherniak@ya.ru>
2022-05-11 16:42:50 +0300

    1

--- final.py
+++ final.py
@@ -5,6 +5,7 @@ from auth_grabber import *
 import random
 import time
 import sys
+import os
 
 start_time = time.time()
 
@@ -13,31 +14,71 @@ with open('auth_data_temp.txt') as f:
 with open('users_tag.txt') as f:
     users = f.read().split()
 
+path = os.getcwd()
+os.chdir('Project data')
 
-for key in range(191, 192):
-    users_list = ['@' + user for user in users]
-    rand_user_str = ''
-    try:
+
+
+with open('follow.txt') as f:
+    b = []
+    for line in f.readlines():
+        b.append(line.rstrip())
+follow_list = [acc.split('\t') for acc in b]
+
+with open('twitter_id.txt') as f:
+    twitter_id_list = []
+    for line in f.readlines():
+        twitter_id_list.append(line.rstrip())
+
+with open('TagNumber.txt') as f:
+    tag_number_list = []
+    for line in f.readlines():
+        tag_number_list.append(line.rstrip())
+
+for project_number in range(len(follow_list)):
+
+    for key in range(1, 3):
+        users_list = ['@' + user for user in users]
+        rand_user_str = ''
+
         print(key)
-        follow_user(data[f'{key}'], get_userid('KahlonEliran'))
-        time.sleep(1)
 
-        #follow_user(data[f'{key}'], '1507713106469961734')
+        for acc_to_follow in follow_list[project_number]:
+            follow_user(data[f'{key}'], acc_to_follow)
 
-        retweet_post(data[f'{key}'], '1517387092711596034')
+        retweet_post(data[f'{key}'], twitter_id_list[project_number])
 
-        like_post(data[f'{key}'], '1517387092711596034')
+        like_post(data[f'{key}'], twitter_id_list[project_number])
 
-        for i in range(3):
+        for i in range(int(tag_number_list[project_number])):
             rand_user = users_list[random.randint(0, len(users_list) - 1)]
             rand_user_str += rand_user + ' '
             users_list.pop(users_list.index(rand_user))
-        comment_tweet(data[f'{key}'], rand_user_str, '1517387092711596034')
-
-
-    except Exception as err:
-        print(f"ОШИБКА В ЦИКЛЕ: {err}")
+        comment_tweet(data[f'{key}'], rand_user_str, twitter_id_list[project_number])
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 #time.sleep(random.uniform(0, 1))
+
+# for key in range(191, 192):
+#     users_list = ['@' + user for user in users]
+#     rand_user_str = ''
+#     try:
+#         print(key)
+#
+#         follow_user(data[f'{key}'], '1507713106469961734')
+#
+#         retweet_post(data[f'{key}'], '1517387092711596034')
+#
+#         like_post(data[f'{key}'], '1517387092711596034')
+#
+#         for i in range(3):
+#             rand_user = users_list[random.randint(0, len(users_list) - 1)]
+#             rand_user_str += rand_user + ' '
+#             users_list.pop(users_list.index(rand_user))
+#         comment_tweet(data[f'{key}'], rand_user_str, '1517387092711596034')
+#
+#
+#
+#     except Exception as err:
+#         print(f"ОШИБКА В ЦИКЛЕ: {err}")
--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from auth_grabber import *
 import itertools
 import random
 from threading import Thread
+from pathlib import Path
+import os
 
 # start_time = time.time()
 #
@@ -42,15 +44,51 @@ from threading import Thread
 # except Exception as err:
 #     print(f"ОШИБКА В ЦИКЛЕ: {err}")
 
-url = 'https://twitter.com/account/access'
-proxies = {
-    "https": "http://t6CauBkh:AhDnyuC3@62.76.7.247:50782"
-}
+# url = 'https://twitter.com/account/access'
+# proxies = {
+#     "https": "http://t6CauBkh:AhDnyuC3@62.76.7.247:50782"
+# }
+#
+# cookie = "ct0=%s; auth_token=%s" % ('1b2acb8f5375ea68838150c7319557af72cc81e18f7d84f2941424316bed375a1be7ca7dbec76942565cfe007ac38be368af242a8167b98bc86a24c265b122743c1c312bf89adbf59d39bc4c283209bd', '2edbeadb1b861cbf2d4b166c45687c5fabb64dbd')
+# user_agent = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
+#     'X-Csrf-Token': '1b2acb8f5375ea68838150c7319557af72cc81e18f7d84f2941424316bed375a1be7ca7dbec76942565cfe007ac38be368af242a8167b98bc86a24c265b122743c1c312bf89adbf59d39bc4c283209bd', 'Content-Type': 'application/x-www-form-urlencoded',
+#     'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA', 'Cookie': cookie}
+# r = requests.get(url, verify=False, headers=user_agent, proxies=proxies)
+# print(r.text)
 
-cookie = "ct0=%s; auth_token=%s" % ('1b2acb8f5375ea68838150c7319557af72cc81e18f7d84f2941424316bed375a1be7ca7dbec76942565cfe007ac38be368af242a8167b98bc86a24c265b122743c1c312bf89adbf59d39bc4c283209bd', '2edbeadb1b861cbf2d4b166c45687c5fabb64dbd')
-user_agent = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    'X-Csrf-Token': '1b2acb8f5375ea68838150c7319557af72cc81e18f7d84f2941424316bed375a1be7ca7dbec76942565cfe007ac38be368af242a8167b98bc86a24c265b122743c1c312bf89adbf59d39bc4c283209bd', 'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA', 'Cookie': cookie}
-r = requests.get(url, verify=False, headers=user_agent, proxies=proxies)
-print(r.text)
+path = os.getcwd()
+os.chdir('Project data')
+
+
+
+with open('follow.txt') as f:
+    b = []
+    for line in f.readlines():
+        b.append(line.rstrip())
+follow_list = [acc.split('\t') for acc in b]
+
+with open('twitter_id.txt') as f:
+    twitter_id_list = []
+    for line in f.readlines():
+        twitter_id_list.append(line.rstrip())
+
+with open('TagNumber.txt') as f:
+    tag_number_list = []
+    for line in f.readlines():
+        tag_number_list.append(line.rstrip())
+
+for project_number in range(len(follow_list)):
+    for my_acc in range(1, 5):
+        for acc_to_follow in follow_list[project_number]:
+            follow_user(data[f'{key}'], acc_to_follow)
+
+        retweet_post(data[f'{key}'], twitter_id_list[project_number])
+
+        like_post(data[f'{key}'], twitter_id_list[project_number])
+
+        for i in range(tag_number_list[project_number]):
+            rand_user = users_list[random.randint(0, len(users_list) - 1)]
+            rand_user_str += rand_user + ' '
+            users_list.pop(users_list.index(rand_user))
+        comment_tweet(data[f'{key}'], rand_user_str, twitter_id_list[project_number])
